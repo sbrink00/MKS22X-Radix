c766b547da6f15b1120c7622ca1ef2fa0fa93fd6
sbrink00 <sbrink00@stuy.edu>
2019-04-01 10:02:28 -0400

    Radix sort now initializes each linked list in the buckets array and finds the largest value in the array

--- Radix.java
+++ Radix.java
@@ -1,3 +1,14 @@
 public class Radix{
-  
+
+  @SuppressWarnings("unchecked")
+  public static void radixsort(int[] data){
+    if (data.length == 0) return;
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
+    for (int idx = 0; idx < 10; idx ++) buckets[idx] = new MyLinkedList<Integer>();
+    int largest = Math.abs(data[0]);
+    for (int idx:data) if (Math.abs(idx) > largest) largest = Math.abs(idx);
+  }
+
+
+
 }

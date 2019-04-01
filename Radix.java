899f9b8bc0dd4a37637726277b5b602c6d2d6ed3
sbrink00 <sbrink00@stuy.edu>
2019-04-01 10:08:19 -0400

    started writing radix srot method in Radix class

--- Radix.java
+++ Radix.java
@@ -7,6 +7,13 @@ public class Radix{
     for (int idx = 0; idx < 10; idx ++) buckets[idx] = new MyLinkedList<Integer>();
     int largest = Math.abs(data[0]);
     for (int idx:data) if (Math.abs(idx) > largest) largest = Math.abs(idx);
+    String num = largest + "";
+    int loopLength = largest.length();
+    for (int idx = 0; idx < loopLength; idx ++){
+      for (int i:data){
+        
+      }
+    }
   }
 
 
--- driver.java
+++ driver.java
@@ -1,15 +1,6 @@
 public class driver{
   public static void main(String[]args){
-    MyLinkedList<Integer> m1 = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> m2 = new MyLinkedList<Integer>();
-    m1.addLast(2);
-    m1.addFirst(1);
-    System.out.println(m1);
-    m2.addLast(4);
-    m2.addFirst(3);
-    System.out.println(m2);
-    m1.extend(m2);
-    System.out.println(m1);
-    System.out.println(m2);
+    int[] ary = {1, 5, 2, -17, 4, -7, -5};
+    Radix.radixsort(ary);
   }
 }
